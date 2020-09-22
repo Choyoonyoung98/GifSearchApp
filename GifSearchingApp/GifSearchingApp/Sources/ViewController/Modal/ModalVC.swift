76bda7136328f5695899d972c16e8130796e23b1
조윤영 <yoon980208@gmail.com>
2020-09-23 01:52:54 +0900

    [CREATE] 모달 화면 레이아웃 구현
    - Gif ImageView 추가
    - Star ImageView 추가
    - Gif Title Label 추가
    - 공유 Button 추가
    - ModalVC 추가

---
+++ GifSearchingApp/GifSearchingApp/Sources/ViewController/Modal/ModalVC.swift
@@ -0,0 +1,30 @@
+//
+//  ModalVC.swift
+//  GifSearchingApp
+//
+//  Created by 조윤영 on 2020/09/23.
+//  Copyright © 2020 조윤영. All rights reserved.
+//
+
+import UIKit
+
+class ModalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
